6e63e0859f7a46aad9028753ce7fcb6fa366dad1
Dan Foster <danfoster@locussoftware.com>
2024-12-09 17:11:13 +0000

    day4

---
+++ src/utils/Point.ts
@@ -0,0 +1,23 @@
+export default class Point {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(value: Point): void {
+        this.x += value.x;
+        this.y += value.y;
+    }
+
+    set(value: Point): void {
+        this.x = value.x;
+        this.y = value.y;
+    }
+
+    clone(): Point {
+        return new Point(this.x, this.y);
+    }
+}
