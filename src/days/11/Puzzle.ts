873884f99b711d40784d8f70ee550e19fb843567
Dan Foster <danfoster@locussoftware.com>
2024-12-13 14:27:08 +0000

    day 11

--- src/days/11/Puzzle.ts
+++ src/days/11/Puzzle.ts
@@ -1,109 +1,81 @@
 import Puzzle from '../../Puzzle';
 
-class Stone {
-    data: number;
-    next: Stone | undefined;
-
-    constructor(data: number) {
-        this.data = data;
-        this.next = null;
-    }
-
-    step(): Stone {
-        if (this.data == 0) {
-            this.data = 1;
-            return this.next;
+function memoizeMethod<T extends (...args: unknown[]) => unknown>(
+    fn: T
+): (this: ThisParameterType<T>, ...args: Parameters<T>) => ReturnType<T> {
+    const cache = new Map<string, ReturnType<T>>();
+    return function (
+        this: ThisParameterType<T>,
+        ...args: Parameters<T>
+    ): ReturnType<T> {
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+            // console.log('Fetching from cache:', key);
+            return cache.get(key) as ReturnType<T>;
         }
-
-        let str_data = this.data.toString();
-        let str_len = str_data.length;
-
-        if (str_len % 2 == 0) {
-            const data1: number = +str_data.slice(0, str_len / 2);
-            const data2: number = +str_data.slice(str_len / 2);
-            const newstone = new Stone(data2);
-            this.data = data1;
-            newstone.next = this.next;
-            this.next = newstone;
-            return newstone.next;
-        }
-
-        this.data = this.data * 2024;
-        return this.next;
-    }
-}
-
-class StoneList {
-    head: Stone;
-    constructor(head: Stone | undefined = null) {
-        this.head = head;
-    }
-
-    size() {
-        let count = 0;
-        let node = this.head;
-        while (node) {
-            count++;
-            node = node.next;
-        }
-        return count;
-    }
-
-    toArray() {
-        const data = [];
-        let node = this.head;
-        while (node) {
-            data.push(node.data);
-            node = node.next;
-        }
-        return data;
-    }
-
-    step(): void {
-        let node = this.head;
-        while (node) {
-            node = node.step();
-        }
-    }
+        // console.log('Computing result for:', key);
+        const result = fn.apply(this, args);
+        cache.set(key, result);
+        return result;
+    };
 }
 
 export default class Day11 extends Puzzle {
     day = '11';
 
-    length: number;
-    stones: StoneList;
+    stones: number[];
+    stoneCache: Map<string, number>;
 
     constructor() {
         super();
+        this.stoneCache = new Map();
     }
 
     parse(input: string) {
         input = input.replace(/\n$/, '');
-        const values = input.split(' ');
-        this.stones = new StoneList();
-        let prevstone = new Stone(+values[0]);
-        this.stones.head = prevstone;
-        for (let i = 1; i < values.length; i++) {
-            const stone = new Stone(+values[i]);
-            prevstone.next = stone;
-            prevstone = stone;
-        }
+        this.stones = input.split(' ').map(Number);
     }
 
-    p1(): number | string {
-        // console.log(this.stones.toArray().join(' '));
-        for (let i = 0; i < 25; i++) {
-            this.stones.step();
-            // console.log(this.stones.toArray().join(' '));
+    stepCount(stone: number, count: number): number {
+        if (count == 0) {
+            return 1;
         }
-        return this.stones.size();
+
+        if (stone == 0) {
+            return this.memoizedStepCount(1, count - 1);
+        }
+
+        let str_data = stone.toString();
+        let str_len = str_data.length;
+
+        if (str_len % 2 == 0) {
+            const data1: number = +str_data.slice(0, str_len / 2);
+            const data2: number = +str_data.slice(str_len / 2);
+            return (
+                this.memoizedStepCount(data1, count - 1) +
+                this.memoizedStepCount(data2, count - 1)
+            );
+        }
+
+        return this.memoizedStepCount(stone * 2024, count - 1);
+    }
+
+    memoizedStepCount = memoizeMethod(this.stepCount);
+
+    p1(): number | string {
+        let total = 0;
+        for (const stone of this.stones) {
+            total += this.memoizedStepCount(stone, 25);
+        }
+        return total;
     }
 
     p2(): number | string {
-        for (let i = 0; i < 50; i++) {
-            this.stones.step();
-            // console.log(this.stones.toArray().join(' '));
+        let total = 0;
+        for (const stone of this.stones) {
+            total += this.memoizedStepCount(stone, 75);
         }
-        return this.stones.size();
+        return total;
+        return this.stones.length;
     }
 }
